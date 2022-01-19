301e16e50319c28617fcee0c8476afe336bb063d
Gergana Roshleva <86190091+GerganaR@users.noreply.github.com>
2022-01-19 19:08:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
+import { useMemo, useState } from "react";
 import "./App.css";
-import React, {useState, useMemo} from "react";
 
 function App() {
-  const [text, setText] = useState('');
-  const checkIfValid = (string) => {
-    const numbers = /^\d+$/;
-    return numbers.test(string);
-  };
+  const [text, setText] = useState("");
+  const [valid, setValid] = useState(false);
+
+  useMemo(() => {
+    checkTextNumber(text);
+  }, [text]);
 
   const validator = useMemo(() => checkIfValid(text));
 
-
   return (
     <div className="App">
-     <div className="control has-icons-right">
+      <div className="control has-icons-right">
         <input
           className="input is-large"
           type="text"
           placeholder="Enter number..."
           value={text}
-          onChange={(e)=> {
-            setText(e.target.value);
-          }}
-
+          onChange={(e) => setText(e.target.value)}
         />
         <span className="icon is-small is-right">
-          <i className={ validator? "fa-check" : "fas fa-times"} />
-        </span> 
+          <i className={valid ? "fas fa-check" : "fas fa-times"} />
+        </span>
       </div>
     </div>
   );
+
+  function checkTextNumber(text) {
+    const reg = new RegExp("^\\d+$");
+    setValid(reg.test(text));
+  }
 }
 
 export default App;
